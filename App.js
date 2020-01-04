a7eaab85bb0b6da52563af4004968771e52998ff
Daniel <daniel_sousa.p@outlook.com>
2020-01-03 22:27:20 -0300

    Prettier e eslint configurados

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+    return (
+        <View>
+            <Text>Meu app</Text>
+            <Text>Meu app</Text>
+        </View>
+    );
+}
