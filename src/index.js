0032e380c8d37c5c2510df4589485194a1f639e8
Daniel <daniel_sousa.p@outlook.com>
2020-01-04 09:56:17 -0300

    React Navigation configurado

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
 import './config/ReactotronConfig';
 
-console.tron.log('Hello World');
+import Routes from './routes';
 
 export default function App() {
-    return (
-        <View>
-            <Text>Meu app</Text>
-            <Text>Meu app</Text>
-        </View>
-    );
+    return <Routes />;
 }
---
+++ src/pages/Main/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Main() {
+    return (
+        <View>
+            <Text>Main</Text>
+        </View>
+    );
+}
+
+Main.navigationOptions = {
+    title: 'Usuários',
+    headerTitleAlign: 'center',
+};
